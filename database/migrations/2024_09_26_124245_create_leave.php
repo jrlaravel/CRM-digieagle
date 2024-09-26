752f05c63e41cde9d825080d7c10e19e4bcc795b
jrlaravel <jrlaravel.digieagleinc@gmail.com>
2024-09-26 13:27:25 +0530

    local changes

---
+++ database/migrations/2024_09_26_124245_create_leave.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leave', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('leave_type_id');
+            $table->date('apply_date');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('reason');
+            $table->string('other');
+            $table->integer('status')->default(0); 
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('leave_type_id')->references('id')->on('leavetype');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leave');
+    }
+};
