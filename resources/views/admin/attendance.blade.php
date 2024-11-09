b85e4df0c46a045be610b767f1098d9ca5f277f6
jrlaravel <jrlaravel.digieagleinc@gmail.com>
2024-11-09 18:09:32 +0530

    local changes

--- resources/views/admin/attendance.blade.php
+++ resources/views/admin/attendance.blade.php
@@ -115,169 +115,146 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.13/jspdf.plugin.autotable.min.js"></script>
 
 <script>
-    $(document).ready(function() {
-        // Handle form submission to fetch data
-        $('#inoutForm').on('submit', function(e) {
-            e.preventDefault(); // Prevent the default form submission behavior
+   $(document).ready(function() {
+    // Handle form submission to fetch data
+    $('#inoutForm').on('submit', function(e) {
+        e.preventDefault();
 
-            let empName = $('#employee option:selected').text(); 
-            let fdate = $('input[name="fdate"]').val();
-            let tdate = $('input[name="tdate"]').val();
+        let empName = $('#employee option:selected').text(); 
+        let fdate = $('input[name="fdate"]').val();
+        let tdate = $('input[name="tdate"]').val();
 
-            let formData = {
-                empcode: $('#employee').val(),
-                fdate: fdate,
-                tdate: tdate,
-                _token: $('meta[name="csrf-token"]').attr('content')
-            };
+        let formData = {
+            empcode: $('#employee').val(),
+            fdate: fdate,
+            tdate: tdate,
+            _token: $('meta[name="csrf-token"]').attr('content')
+        };
 
-            $.ajax({
-                type: 'POST',
-                url: $(this).attr('action'),
-                data: formData,
-                success: function(response) {
+        $.ajax({
+            type: 'POST',
+            url: $(this).attr('action'),
+            data: formData,
+            success: function(response) {
+                console.log(response);
+                var tableBody = $('#inOutPunchTable tbody');
+                tableBody.empty();
 
-                    console.log(response);
-                    var tableBody = $('#inOutPunchTable tbody');
-                    tableBody.empty(); // Clear previous data
-
-                    if (response['InOutPunchData'].length === 0) {
-                        alert('No data available for the selected date range.');
-                        return;
-                    }
-
-                    var totalWorkMinutes = 0;
-                    var totalOTMinutes = 0;
-
-
-                    // Populate the table with fetched data
-                    $.each(response['InOutPunchData'], function(index, item) {
-                        var status = item.INTime === '--:--' ? 'A' : (item.OUTTime === '--:--' ? 'Not Punch Out' : 'P');
-                        var row = '<tr>' +
-                            '<td>' + (index + 1) + '</td>' + 
-                            '<td>' + item.Name + '</td>' +
-                            '<td>' + item.DateString + '</td>' +
-                            '<td>' + item.INTime + '</td>' +
-                            '<td >' + item.OUTTime + '</td>' +
-                            '<td class="text-danger">' + item.Erl_Out + '</td>' +
-                            '<td>' + item.WorkTime + '</td>' +
-                            '<td class="text-danger">' + item.OverTime + '</td>' +
-                            '<td>' + status + '</td>' +
-                            '</tr>';
-                             tableBody.append(row);
-
-                             // Calculate total work minutes
-                            var workTime = item.WorkTime.split(':');
-                            var workHours = parseInt(workTime[0]);
-                            var workMinutes = parseInt(workTime[1]);
-                            totalWorkMinutes += (workHours * 60) + workMinutes;
-
-                            var overtime = item.OverTime.split(':');
-                            var otHours = parseInt(overtime[0]);
-                            var otMinutes = parseInt(overtime[1]);
-                            totalOTMinutes += (otHours * 60) + otMinutes;
-
-                        }); 
-
-                            // Convert total work minutes to hours and minutes
-                            $('#totalWorkHours').show();
-
-                            var totalWorkHours = Math.floor(totalWorkMinutes / 60);
-                            var remainingWorkMinutes = totalWorkMinutes % 60;
-                            $('#workHours').text(totalWorkHours + ' hrs ' + remainingWorkMinutes + ' mins');
-
-                            var totalOTHours = Math.floor(totalOTMinutes / 60);
-                            var remainingOTMinutes = totalOTMinutes % 60;
-                            $('#otHours').text(totalOTHours + ' hrs ' + remainingOTMinutes + ' mins');
-
-                            $('#download-button').data('empName', empName);
-                            $('#download-button').data('dateRange', fdate + " to " + tdate);
-                            $('#download-button').data('totalHours', totalWorkHours + ' hrs ' + remainingWorkMinutes + ' mins');
-
-                },
-                error: function(error) {
-                    alert('An error occurred while fetching data.');
-                    console.log(error);
+                if (response['InOutPunchData'].length === 0) {
+                    alert('No data available for the selected date range.');
+                    return;
                 }
-            });
-        });
 
-        // Handle PDF generation after data is loaded
-        $('#download-button').click(function() {    
-            if ($('#inOutPunchTable tbody tr').length === 0) {
-                alert('No data available to generate PDF.');
-                $('#totalWorkHours').hide(); // Hide the total work hours div if there's no data
-                return;
-            }
+                var totalWorkMinutes = 0;
+                var totalOTMinutes = 0;
 
-            // Retrieve employee details
-            let empName = $(this).data('empName');
-            let dateRange = $(this).data('dateRange');
-            let totalHours = $(this).data('totalHours'); // Retrieve total hours
+                $.each(response['InOutPunchData'], function(index, item) {
+                    var status = item.INTime === '--:--' ? 'A' : (item.OUTTime === '--:--' ? 'Not Punch Out' : 'P');
+                    var row = '<tr>' +
+                        '<td>' + (index + 1) + '</td>' + 
+                        '<td>' + item.Name + '</td>' +
+                        '<td>' + item.DateString + '</td>' +
+                        '<td>' + item.INTime + '</td>' +
+                        '<td >' + item.OUTTime + '</td>' +
+                        '<td class="text-danger">' + item.Erl_Out + '</td>' +
+                        '<td>' + item.WorkTime + '</td>' +
+                        '<td class="text-danger">' + item.OverTime + '</td>' +
+                        '<td>' + status + '</td>' +
+                        '</tr>';
+                    tableBody.append(row);
 
-            const { jsPDF } = window.jspdf;
-            const pdf = new jsPDF('p', 'pt', 'a4');
+                    var workTime = item.WorkTime.split(':');
+                    var workHours = parseInt(workTime[0]);
+                    var workMinutes = parseInt(workTime[1]);
+                    totalWorkMinutes += (workHours * 60) + workMinutes;
 
-            // Define the table headers and data
-            const headers = [];
-            const data = [];
-           
-            // Get table headers
-            $('#inOutPunchTable thead tr th').each(function() {
-                headers.push($(this).text());
-            });
-
-            // Get table body data and calculate totals for Worktime and Overtime
-            $('#inOutPunchTable tbody tr').each(function() {
-                const row = [];
-                $(this).find('td').each(function(index) {
-                    const cellText = $(this).text();
-                    row.push(cellText);
-
-                    // Assuming Worktime is in column 5 and Overtime is in column 6
-                    if (index === 6) { // Adjust the index if necessary
-                        totalWorktimeMinutes += convertTimeToMinutes(cellText);
-                    } 
+                    var overtime = item.OverTime.split(':');
+                    var otHours = parseInt(overtime[0]);
+                    var otMinutes = parseInt(overtime[1]);
+                    totalOTMinutes += (otHours * 60) + otMinutes;
                 });
-                data.push(row);
-            });
 
-           
-            // Add employee details at the top of the PDF
-            pdf.setFontSize(12);
-            pdf.text("Employee Name: " + empName, 40, 30);
-            pdf.text("Date Range: " + dateRange, 40, 50);
-            pdf.text("Total Work Hours: " + totalHours, 40, 70);
+                $('#totalWorkHours').show();
 
-            // Use autoTable to add the table with headers and data
-            pdf.autoTable({
-                head: [headers],
-                body: data,
-                startY: 90, // Start table below the employee details
-                margin: { top: 50, left: 20, right: 20 },
-                theme: 'striped',
-                styles: {
-                    fontSize: 8,
-                    cellPadding: 3,
-                },
-                headStyles: {
-                    fillColor: [52, 58, 64],
-                    textColor: [255, 255, 255],
-                },
-                didDrawPage: function(data) {
-                    // Footer on each page
-                    const pageCount = pdf.internal.getNumberOfPages();
-                    pdf.setFontSize(10);
-                    pdf.text(`Page ${data.pageNumber} of ${pageCount}`, pdf.internal.pageSize.width - 40, pdf.internal.pageSize.height - 10);
-                },
-            });
+                var totalWorkHours = Math.floor(totalWorkMinutes / 60);
+                var remainingWorkMinutes = totalWorkMinutes % 60;
+                $('#workHours').text(totalWorkHours + ' hrs ' + remainingWorkMinutes + ' mins');
 
-        
+                var totalOTHours = Math.floor(totalOTMinutes / 60);
+                var remainingOTMinutes = totalOTMinutes % 60;
+                $('#otHours').text(totalOTHours + ' hrs ' + remainingOTMinutes + ' mins');
 
-            // Save the PDF
-            pdf.save(empName + "_attendance_report.pdf");
+                $('#download-button').data('empName', empName);
+                $('#download-button').data('dateRange', fdate + " to " + tdate);
+                $('#download-button').data('totalHours', totalWorkHours + ' hrs ' + remainingWorkMinutes + ' mins');
+            },
+            error: function(error) {
+                alert('An error occurred while fetching data.');
+                console.log(error);
+            }
         });
     });
+
+    // Handle PDF generation
+    $('#download-button').click(function() {
+        if ($('#inOutPunchTable tbody tr').length === 0) {
+            alert('No data available to generate PDF.');
+            $('#totalWorkHours').hide();
+            return;
+        }
+
+        let empName = $(this).data('empName');
+        let dateRange = $(this).data('dateRange');
+        let totalHours = $(this).data('totalHours');
+
+        const { jsPDF } = window.jspdf;
+        const pdf = new jsPDF('p', 'pt', 'a4');
+
+        const headers = [];
+        const data = [];
+       
+        $('#inOutPunchTable thead tr th').each(function() {
+            headers.push($(this).text());
+        });
+
+        $('#inOutPunchTable tbody tr').each(function() {
+            const row = [];
+            $(this).find('td').each(function() {
+                row.push($(this).text());
+            });
+            data.push(row);
+        });
+
+        pdf.setFontSize(12);
+        pdf.text("Employee Name: " + empName, 40, 30);
+        pdf.text("Date Range: " + dateRange, 40, 50);
+        pdf.text("Total Work Hours: " + totalHours, 40, 70);
+
+        pdf.autoTable({
+            head: [headers],
+            body: data,
+            startY: 90,
+            margin: { top: 50, left: 20, right: 20 },
+            theme: 'striped',
+            styles: {
+                fontSize: 8,
+                cellPadding: 3,
+            },
+            headStyles: {
+                fillColor: [52, 58, 64],
+                textColor: [255, 255, 255],
+            },
+            didDrawPage: function(data) {
+                const pageCount = pdf.internal.getNumberOfPages();
+                pdf.setFontSize(10);
+                pdf.text(`Page ${data.pageNumber} of ${pageCount}`, pdf.internal.pageSize.width - 40, pdf.internal.pageSize.height - 10);
+            },
+        });
+
+        pdf.save(empName + "_attendance_report.pdf");
+    });
+});
+
 </script>
 
 @endsection
