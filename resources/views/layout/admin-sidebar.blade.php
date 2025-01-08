e6abaae774dd20085ef6255a02f7560fb4770a12
jrlaravel <jrlaravel.digieagleinc@gmail.com>
2025-01-08 16:42:50 +0530

    local changes

--- resources/views/layout/admin-sidebar.blade.php
+++ resources/views/layout/admin-sidebar.blade.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="icon" type="image/x-icon" href="{{asset('storage\img\logo\Digieagle-Favicon.png')}}">
+	<link rel="icon" type="image/x-icon" href="{{asset('storage\logo\Digieagle-Favicon.png')}}">
 	<link rel="preconnect" href="https://fonts.gstatic.com/">
 
 	<title>Dashboard | Digieagle INC</title>
